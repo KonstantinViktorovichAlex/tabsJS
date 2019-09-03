459e06de7758a7c3d11243e13b33aeef8ff67b56
Konstantin <akonstantin228@gmail.com>
2019-09-03 12:50:16 +0300

    tabs

---
+++ index.js
@@ -0,0 +1,23 @@
+function init() {
+  let tabBody = document.querySelectorAll(".tab-body");
+  for (i = 1; i < tabBody.length; i++) {
+    tabBody[i].style.display = "none";
+  }
+}
+init();
+
+let tab = document.querySelectorAll(".tab");
+tab.forEach(function(element) {
+  element.onclick = showTabs;
+});
+
+function showTabs() {
+  let data = this.getAttribute("data");
+
+  let tabBody = document.querySelectorAll(".tab-body");
+  for (i = 0; i < tabBody.length; i++) {
+    tabBody[i].style.display = "none";
+  }
+  document.querySelector(`.tab-body[data = '${data}'] `).style.display =
+    "block";
+}
